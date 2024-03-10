2e428f3f4fd61a343c9ab833e3fdc46755c7f5b9
Preeti Gusain <preetigusain9173@gmail.com>
2024-03-10 17:28:36 +0530

    added logic for number input

---
+++ index.js
@@ -0,0 +1,60 @@
+let curr_value = 0;
+
+//Number buttons
+const zero = document.querySelector('#zero');
+const one = document.querySelector('#one');
+const two = document.querySelector('#two');
+const three = document.querySelector('#three');
+const four = document.querySelector('#four');
+const five = document.querySelector('#five');
+const six = document.querySelector('#six');
+const seven = document.querySelector('#seven');
+const eight = document.querySelector('#eight');
+const nine = document.querySelector('#nine');
+
+//Arithmetic buttons
+const divide = document.querySelector('#divide');
+const multiply = document.querySelector('#multiply');
+const subtract = document.querySelector('#subtract');
+const add = document.querySelector('#add');
+const equals = document.querySelector('#equals');
+
+//Calculator buttons
+const AC = document.querySelector('#AC');
+const C = document.querySelector('#C');
+const modulus = document.querySelector('#modulus');
+
+//screen in calculator
+const screen_text = document.querySelector('#screen_text');
+
+//Taking number inputs
+zero.onclick = function(){input_number(0)};
+one.onclick = function(){input_number(1)};
+two.onclick = function(){input_number(2)};
+three.onclick = function(){input_number(3)};
+four.onclick = function(){input_number(4)};
+five.onclick = function(){input_number(5)};
+six.onclick = function(){input_number(6)};
+seven.onclick = function(){input_number(7)};
+eight.onclick = function(){input_number(8)};
+nine.onclick = function(){input_number(9)};
+
+//Shows number taken as input on screen
+function input_number(number){
+    console.log("input_number called with " + number);
+    if(curr_value === 0){
+        update_screen(number);
+    }
+    else{
+        let new_value = (curr_value * 10) + number;
+        update_screen(new_value);
+    }
+}
+
+//Takes string as input and sets it to screen
+function update_screen(new_value){
+    console.log("update_screen called with " + new_value);
+    curr_value = new_value;
+    screen_text.value = new_value.toString();
+}
+
